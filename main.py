939af3aa61a3a17214833f12bbebcdb13893b347
Jasper Boogaerdt t Hooft <jasper17@live.nl>
2021-02-17 20:54:02 +0100

    finished sell_product()

--- main.py
+++ main.py
@@ -82,7 +82,6 @@ def buy_product():
         reader = csv.reader(inp)
         writer = csv.writer(out)
         args = get_arguments()
-        # product = args.product.lower()
         id_buy = id(1)
 
         isAdded = False
@@ -121,6 +120,66 @@ def buy_product():
                 args.expiration_date,
             ]
             writer.writerow(new_arr_for_csvfile)
+            try:
+                os.rename("bought_edit.csv", "bought.csv")
+            except:
+                None
+
+
+# SELL PRODUCTS
+def sell_products():
+    with open("bought.csv", "r") as inp, open("bought_edit.csv", "w") as out, open(
+        "sold.csv", "a"
+    ) as sold:
+        reader = csv.reader(inp)
+        writer = csv.writer(out)
+        sold_writer = csv.writer(sold)
+        args = get_arguments()
+        isAdded = False
+
+        for line in reader:
+            id_buy = line[0]
+
+            if args.product == line[2]:
+                if int(line[4]) >= args.amount:
+                    new_amount = int(line[4]) - int(args.amount)
+                    profit_product = args.sell_price - float(line[3])
+
+                    new_amount_arr_for_csvfile = [
+                        line[0],
+                        line[1],
+                        line[2],
+                        line[3],
+                        new_amount,
+                        line[5],
+                    ]
+                    isAdded = True
+
+                    if new_amount == 0:
+                        None
+                    else:
+                        writer.writerow(new_amount_arr_for_csvfile)
+
+                    arr_for_soldfile = [
+                        id_buy,
+                        display_today,
+                        args.product,
+                        args.amount,
+                        args.sell_price,
+                        profit_product,
+                    ]
+                    sold_writer.writerow(arr_for_soldfile)
+                    try:
+                        os.rename("bought_edit.csv", "bought.csv")
+                    except:
+                        None
+                else:
+                    print("Amount is more than we have in stock")
+            elif args.product != line[2]:
+                writer.writerow(line)
+
+        if not isAdded:
+            writer.writerow(line)
             try:
                 os.rename("bought_edit.csv", "bought.csv")
             except:
@@ -208,63 +267,6 @@ def get_report():
             for line in sold_report:
                 sum_profit += float(line[4])
             print(sum_profit)
-
-
-# Ik moet de sell functie hetzelfde inrichten als de buy functie. het is vergelijkbaar,
-# alleen het overzetten naar de sold file is anders
-
-# SELL PRODUCTS
-def sell_products():
-    args = get_arguments()
-    breakpoint()
-    with open("bought.csv", "r") as inp, open("bought_edit.csv", "w") as out, open(
-        "sold.csv", "a"
-    ) as sold:
-        writer = csv.writer(out)
-        sold_writer = csv.writer(sold)
-
-        for item in csv.reader(inp):
-            id_buy = item[0]
-
-            if args.product == item[2]:
-                if int(item[4]) > args.amount:
-                    new_amount = int(item[4]) - int(args.amount)
-                    profit_product = args.sell_price - float(item[3])
-
-                    new_str_for_csvfile = [
-                        "id_buy",
-                        "buy_date",
-                        "product",
-                        "buy_price",
-                        "amount",
-                        "expiration_date",
-                    ]
-                    new_arr_for_csvfile = [
-                        item[0],
-                        item[1],
-                        item[2],
-                        item[3],
-                        new_amount,
-                        item[5],
-                    ]
-
-                    writer.writerow(new_str_for_csvfile)
-                    writer.writerow(new_arr_for_csvfile)
-
-                    arr_for_soldfile = [
-                        id_buy,
-                        display_today,
-                        args.product,
-                        args.sell_price,
-                        profit_product,
-                    ]
-                    sold_writer.writerow(arr_for_soldfile)
-                    os.remove("bought.csv")
-                    os.rename("bought_edit.csv", "bought.csv")
-                else:
-                    print("Amount is more than we have in stock")
-            elif args.product != item[2]:
-                print("We're out of stock of this product")
 
 
 if __name__ == "__main__":
